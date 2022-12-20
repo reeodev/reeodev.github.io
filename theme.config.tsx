44b9932f6e27af88bfd6ff75bac1e6d870daedca
reemo <r48n34@gmail.com>
2022-12-20 11:37:57 +0800

    Update theme.config.tsx

--- theme.config.tsx
+++ theme.config.tsx
@@ -1,8 +1,5 @@
 import React from 'react'
 import { DocsThemeConfig } from 'nextra-theme-docs'
-// import logo from "./public/img/reelogo.ico"
-
-// app.useStatic("upload")
 
 const config: DocsThemeConfig = {
   logo: (
@@ -17,6 +14,7 @@ const config: DocsThemeConfig = {
   docsRepositoryBase: 'https://github.com/reeodev/reeodev.github.io',
   head: (
     <>
+      <title>Reeodev</title>
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       <meta property="og:title" content="Reeodev" />
       <meta property="og:description" content="For your design and development references." />
@@ -24,6 +22,9 @@ const config: DocsThemeConfig = {
       <meta property="og:type" content="website"/>
       <meta property="og:image" content="https://reeodev.github.io/img/reelogo.ico"/>
       <meta property="og:image:alt" content="Studio logo"/>
+
+      <link rel="mask-icon" href="img/reelogo.ico" color="#5bbad5"></link>
+      <link rel="icon" type="image/x-icon" href="/img/reelogo.ico"></link>
 
       <meta property="og:url" content="https://reeodev.github.io/"></meta>
     </>
